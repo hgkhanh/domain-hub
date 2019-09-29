0a24fa8ef9593a8d4f398d07d084e66f21cdf4a6
Khanh Nguyen <khanh.nguyen@frosmo.com>
2019-09-30 00:07:12 +0300

    bg color, link color

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a href='/seven'>Seven Wonders</a>
+        <a className='App-link' href='/seven'>Seven Wonders</a>
       </header>
     </div>
   );
